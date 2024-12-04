1b6270769c3ed758ab44fdf86dc4f50ddeee9e77
soyilai@proton.me <soyilai@proton.me>
2024-12-03 23:39:11 -0600

    fix pfp: mine

--- src/lib/Members.svelte
+++ src/lib/Members.svelte
@@ -7,51 +7,119 @@
   <div class="bg-ctp-surface0 grid md:grid-cols-2 rounded-sm">
     <div class="flex items-center flex-col mt-4">
       <div class="flex flex-col justify-center items-center mb-2">
-        <img src="https://cdn.discordapp.com/avatars/768761151739658240/d02bfca1a2559b7c9c9bbcc627532cf0.webp" alt="Rivest's Profile Picture" class="rounded-full h-12 w-12" />
+        <img
+          src="https://cdn.discordapp.com/avatars/768761151739658240/d02bfca1a2559b7c9c9bbcc627532cf0.webp"
+          alt="Rivest's Profile Picture"
+          class="rounded-full h-12 w-12"
+        />
         <p class="font-bold text-2xl text-center">Rivest</p>
       </div>
-      <pre class="bg-ctp-base rounded-md p-2 m-2 whitespace-pre-wrap break-words">
+      <pre
+        class="bg-ctp-base rounded-md p-2 m-2 whitespace-pre-wrap break-words">
         <code class="font-mono text-xs md:text-sm p-2">
-<span class="text-ctp-blue">start_profile</span><span class="text-ctp-red">();</span>
+<span class="text-ctp-blue">start_profile</span><span class="text-ctp-red"
+            >();</span
+          >
 
-<span class="text-ctp-blue">set_title</span><span class="text-ctp-red">(</span><span class="text-ctp-peach">"low-level developer"</span><span class="text-ctp-red">);</span>
-<span class="text-ctp-blue">set_pronoun</span><span class="text-ctp-red">(</span><span class="text-ctp-mauve">NULL</span><span class="text-ctp-red">);</span> <span class="text-ctp-green">// whatever</span>
-<span class="text-ctp-blue">add_email</span><span class="text-ctp-red">(</span><span class="text-ctp-peach">"rivest@chimoteam.eu.org"</span><span class="text-ctp-red">);</span>
-<span class="text-ctp-blue">start_skillset</span><span class="text-ctp-red">();</span>
-  <span class="text-ctp-blue">add_skill</span><span class="text-ctp-red">(</span><span class="text-ctp-peach">"C(++)"</span><span class="text-ctp-red">);</span>
-  <span class="text-ctp-blue">add_skill</span><span class="text-ctp-red">(</span><span class="text-ctp-peach">"JavaScript"</span><span class="text-ctp-red">);</span>
-  <span class="text-ctp-blue">add_skill</span><span class="text-ctp-red">(</span><span class="text-ctp-peach">"Embebbed systems"</span><span class="text-ctp-red">);</span>
-<span class="text-ctp-blue">end_skillset</span><span class="text-ctp-red">();</span>
-<span class="text-ctp-blue">add_quirk</span><span class="text-ctp-red">(</span><span class="text-ctp-peach">"retro nerd"</span><span class="text-ctp-red">);</span>
-<span class="text-ctp-blue">set_url</span><span class="text-ctp-red">(</span><span class="text-ctp-peach">"github"</span><span class="text-ctp-red">, </span><span class="text-ctp-peach">"https://github.com/rivest-oss"</span><span class="text-ctp-red">);</span>
+<span class="text-ctp-blue">set_title</span><span class="text-ctp-red">(</span
+          ><span class="text-ctp-peach">"low-level developer"</span><span
+            class="text-ctp-red">);</span
+          >
+<span class="text-ctp-blue">set_pronoun</span><span class="text-ctp-red">(</span
+          ><span class="text-ctp-mauve">NULL</span><span class="text-ctp-red"
+            >);</span
+          > <span class="text-ctp-green">// whatever</span>
+<span class="text-ctp-blue">add_email</span><span class="text-ctp-red">(</span
+          ><span class="text-ctp-peach">"rivest@chimoteam.eu.org"</span><span
+            class="text-ctp-red">);</span
+          >
+<span class="text-ctp-blue">start_skillset</span><span class="text-ctp-red"
+            >();</span
+          >
+  <span class="text-ctp-blue">add_skill</span><span class="text-ctp-red">(</span
+          ><span class="text-ctp-peach">"C(++)"</span><span class="text-ctp-red"
+            >);</span
+          >
+  <span class="text-ctp-blue">add_skill</span><span class="text-ctp-red">(</span
+          ><span class="text-ctp-peach">"JavaScript"</span><span
+            class="text-ctp-red">);</span
+          >
+  <span class="text-ctp-blue">add_skill</span><span class="text-ctp-red">(</span
+          ><span class="text-ctp-peach">"Embebbed systems"</span><span
+            class="text-ctp-red">);</span
+          >
+<span class="text-ctp-blue">end_skillset</span><span class="text-ctp-red"
+            >();</span
+          >
+<span class="text-ctp-blue">add_quirk</span><span class="text-ctp-red">(</span
+          ><span class="text-ctp-peach">"retro nerd"</span><span
+            class="text-ctp-red">);</span
+          >
+<span class="text-ctp-blue">set_url</span><span class="text-ctp-red">(</span
+          ><span class="text-ctp-peach">"github"</span><span
+            class="text-ctp-red"
+            >, </span><span class="text-ctp-peach"
+            >"https://github.com/rivest-oss"</span
+          ><span class="text-ctp-red">);</span>
 
-<span class="text-ctp-blue">end_profile</span><span class="text-ctp-red">();</span>
+<span class="text-ctp-blue">end_profile</span><span class="text-ctp-red"
+            >();</span
+          >
         </code>
       </pre>
     </div>
     <div class="flex justify-center items-center flex-col mt-4">
       <div class="flex flex-col justify-center items-center mb-2">
-        <img src="https://cdn.discordapp.com/avatars/608029420929679370/82404a95956db885fee852a7bc15a450.webp" alt="Ilai's Profile Picture" class="rounded-full h-12 w-12" />
+        <img
+          src="https://cdn.discordapp.com/avatars/608029420929679370/0f2ef3e89102c5b769a43257b5b1d477.webp"
+          alt="Ilai's Profile Picture"
+          class="rounded-full h-12 w-12"
+        />
         <p class="font-bold text-2xl text-center">Ilai</p>
       </div>
-      <pre class="bg-ctp-base rounded-md p-2 m-2 whitespace-pre-wrap break-words">
+      <pre
+        class="bg-ctp-base rounded-md p-2 m-2 whitespace-pre-wrap break-words">
         <code class="font-mono text-xs md:text-sm p-2">
 <span class="text-ctp-red">&lt;div&gt;</span>
-  <span class="text-ctp-red">&lt;title&gt;</span>front-end developer<span class="text-ctp-red">&lt;/title&gt;</span>
-    <span class="text-ctp-red">&lt;pronoun&gt;</span>he/him<span class="text-ctp-red">&lt;/pronoun&gt;</span>
-    <span class="text-ctp-red">&lt;email&gt;</span>ilai@chimoteam.eu.org<span class="text-ctp-red">&lt;/pronoun&gt;</span>
+  <span class="text-ctp-red">&lt;title&gt;</span>front-end developer<span
+            class="text-ctp-red">&lt;/title&gt;</span
+          >
+    <span class="text-ctp-red">&lt;pronoun&gt;</span>he/him<span
+            class="text-ctp-red">&lt;/pronoun&gt;</span
+          >
+    <span class="text-ctp-red">&lt;email&gt;</span>ilai@chimoteam.eu.org<span
+            class="text-ctp-red">&lt;/pronoun&gt;</span
+          >
     <span class="text-ctp-red">&lt;skillset&gt;</span>
     <span class="text-ctp-red">&lt;ul&gt;</span>
-      <span class="text-ctp-red">&lt;li&gt;</span>JavaScript<span class="text-ctp-red">&lt;/li&gt;</span>
-      <span class="text-ctp-red">&lt;li&gt;</span>TypeScript<span class="text-ctp-red">&lt;/li&gt;</span>
-      <span class="text-ctp-red">&lt;li&gt;</span>HTML<span class="text-ctp-red">&lt;/li&gt;</span>
-      <span class="text-ctp-red">&lt;li&gt;</span>CSS<span class="text-ctp-red">&lt;/li&gt;</span>
-      <span class="text-ctp-red">&lt;li&gt;</span>Svelte<span class="text-ctp-red">&lt;/li&gt;</span>
-      <span class="text-ctp-red">&lt;li&gt;</span>React<span class="text-ctp-red">&lt;/li&gt;</span>
+      <span class="text-ctp-red">&lt;li&gt;</span>JavaScript<span
+            class="text-ctp-red">&lt;/li&gt;</span
+          >
+      <span class="text-ctp-red">&lt;li&gt;</span>TypeScript<span
+            class="text-ctp-red">&lt;/li&gt;</span
+          >
+      <span class="text-ctp-red">&lt;li&gt;</span>HTML<span class="text-ctp-red"
+            >&lt;/li&gt;</span
+          >
+      <span class="text-ctp-red">&lt;li&gt;</span>CSS<span class="text-ctp-red"
+            >&lt;/li&gt;</span
+          >
+      <span class="text-ctp-red">&lt;li&gt;</span>Svelte<span
+            class="text-ctp-red">&lt;/li&gt;</span
+          >
+      <span class="text-ctp-red">&lt;li&gt;</span>React<span
+            class="text-ctp-red">&lt;/li&gt;</span
+          >
     <span class="text-ctp-red">&lt;/ul&gt;</span>
   <span class="text-ctp-red">&lt;/skillset&gt;</span>
-  <span class="text-ctp-red">&lt;github&gt;</span><a href="https://github.com/ilaicraftYT" class="font-bold underline">https://github.com/ilaicraftYT</a><span class="text-ctp-red">&lt;/github&gt;</span>
-  <span class="text-ctp-red">&lt;website&gt;</span><a href="https://soyilai.vercel.app" class="font-bold underline">https://soyilai.vercel.app</a><span class="text-ctp-red">&lt;/website&gt;</span>
+  <span class="text-ctp-red">&lt;github&gt;</span><a
+            href="https://github.com/ilaicraftYT"
+            class="font-bold underline">https://github.com/ilaicraftYT</a
+          ><span class="text-ctp-red">&lt;/github&gt;</span>
+  <span class="text-ctp-red">&lt;website&gt;</span><a
+            href="https://soyilai.vercel.app"
+            class="font-bold underline">https://soyilai.vercel.app</a
+          ><span class="text-ctp-red">&lt;/website&gt;</span>
 <span class="text-ctp-red">&lt;/div&gt;</span>
         </code>
       </pre>
